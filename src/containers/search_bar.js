016cbb6eee40d6b872aab2ad15e94f6101eafa07
zaturnvalley <zacharycmason@gmail.com>
2016-12-06 19:03:27 -0800

    added submit button

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -6,6 +6,10 @@ export default class SearchBar extends Component {
       <form className="input-group">
         <input />
         <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">
+            Submit
+          </button>
+        </span>
       </form>
     );
   }
