1e1d40a8f9052497111dced1e00ba1276603aedd
zaturnvalley <zacharycmason@gmail.com>
2016-12-06 19:02:23 -0800

    added span to searchbar

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react';
 export default class SearchBar extends Component {
   render() {
     return (
-      <form className='input-group'>
+      <form className="input-group">
         <input />
+        <span className="input-group-btn">
       </form>
     );
   }
