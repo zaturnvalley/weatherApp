7226f58f8c278f17fa6dc8f879c4dcd62aba7bc0
zaturnvalley <zacharycmason@gmail.com>
2016-12-07 17:40:37 -0800

    constructor for state

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
       <form className="input-group">
