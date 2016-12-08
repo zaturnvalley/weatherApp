057db9b5d6ab187fc0fd402cfd0f829c3649ef9f
zaturnvalley <zacharycmason@gmail.com>
2016-12-07 17:41:09 -0800

    this.state term set to empty string

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 export default class SearchBar extends Component {
   constructor(props) {
     super(props);
+
+    this.state = { term: '' };
   }
   render() {
     return (
