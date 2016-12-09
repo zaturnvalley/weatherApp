d27b0780ea866373056ac4a3830acbaed118c613
zaturnvalley <zacharycmason@gmail.com>
2016-12-09 15:56:32 -0800

    root url const + api key

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,6 @@
 const API_KEY = 'a9ab0355639c8c50c6fcd6a63ccb22d7';
+//uses ES6 syntax with ${API_KEY} instead of + API_KEY
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
