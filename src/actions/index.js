0038e03bc1070fe06c5e76d4d084ccd101dcbf7a
zaturnvalley <zacharycmason@gmail.com>
2016-12-09 15:59:21 -0800

    const url pulls in city into root url from param

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,9 @@ const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KE
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
-export function fetchWeather() {
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+
   return {
     type: FETCH_WEATHER
   };
