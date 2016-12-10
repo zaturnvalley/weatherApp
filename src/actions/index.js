22a81bfa744f4bf0d072ba4eb2823fe5f3a08b05
zaturnvalley <zacharycmason@gmail.com>
2016-12-09 16:02:01 -0800

    get request on url via axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const API_KEY = 'a9ab0355639c8c50c6fcd6a63ccb22d7';
 //uses ES6 syntax with ${API_KEY} instead of + API_KEY
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
@@ -6,7 +8,8 @@ export const FETCH_WEATHER = 'FETCH_WEATHER'
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
-
+  const request = axios.get(url);
+  
   return {
     type: FETCH_WEATHER
   };
