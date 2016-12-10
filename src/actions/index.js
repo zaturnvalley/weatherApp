6af9aa328c90a6696eff62422301765e759dfa56
zaturnvalley <zacharycmason@gmail.com>
2016-12-09 16:03:48 -0800

    request set as value to payload key, returned from action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,8 +9,9 @@ export const FETCH_WEATHER = 'FETCH_WEATHER'
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
-  
+
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   };
 }
