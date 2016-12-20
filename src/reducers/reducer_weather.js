83b0d76c35707fb21342e77bdca521be5a62370c
zaturnvalley <zacharycmason@gmail.com>
2016-12-20 15:58:33 -0800

    returns new instance of state concated

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -3,7 +3,8 @@ import { FETCH_WEATHER } from '../actions/index';
 export default function(state = [], action){  
   switch (action.type) {
     case FETCH_WEATHER: 
-      return [ action.payload.data ];
+      // returns new instance of state. do not just push
+      return state.concat([action.payload.data]);
   }
   return state;
 }
