3f6737f3ffbaeca6a3c61261f431c3996620b5bf
zaturnvalley <zacharycmason@gmail.com>
2016-12-20 16:00:10 -0800

    es6 syntax of concat state

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -4,7 +4,9 @@ export default function(state = [], action){
   switch (action.type) {
     case FETCH_WEATHER: 
       // returns new instance of state. do not just push
-      return state.concat([action.payload.data]);
+      // es6 way to write same thing, flattens array
+      // [city, city, city ] NOT [city,[city,[city]]]
+      return [ action.payload.data, ...state ];
   }
   return state;
 }
