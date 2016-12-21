d791deddfbe5c24247ad60e8d9712133e8bb18eb
zaturnvalley <zacharycmason@gmail.com>
2016-12-21 13:00:29 -0800

    exporting connected mapstatetoprops with weatherlist comp

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-export default class WeatherList extends Component {
+class WeatherList extends Component {
   render() {
     return (
       <table className="table table-hover">
@@ -22,6 +22,10 @@ export default class WeatherList extends Component {
   }
 }
 
-function mapStateToProps(state){
-  return { weather: state.weather };
+// weather = const weather = state.weather for param
+// es6 shortcuts
+function mapStateToProps({ weather }){
+  return { weather }; // { weather } === { weather : weather }
 }
+
+export default connect(mapStateToProps)(WeatherList);
