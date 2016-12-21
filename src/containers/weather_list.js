88c557151071bf08f7b7726fff53b846d0e2ba62
zaturnvalley <zacharycmason@gmail.com>
2016-12-21 12:47:24 -0800

    table structure, header says City

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -4,7 +4,13 @@ export default class WeatherList extends Component {
   render() {
     return (
       <table className="table table-hover">
-        
+        <thead>
+          <tr>
+            <th>City</th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
       </table>
     );
   }
