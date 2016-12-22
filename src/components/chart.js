79e0e76b95e13cb70cd732de9e787ad246c80884
zaturnvalley <zacharycmason@gmail.com>
2016-12-21 17:17:52 -0800

    props.data and color for config

--- src/components/chart.js
+++ src/components/chart.js
@@ -4,8 +4,8 @@ import { Sparklines, SparklinesLine } from 'react-sparklines';
 export default (props) => {
   return (
     <div>
-      <Sparklines height={120} width={180} data={temps}>
-        <SparklinesLine color="red" />
+      <Sparklines height={120} width={180} data={props.data}>
+        <SparklinesLine color={props.color} />
       </Sparklines>
     </div>
   );
