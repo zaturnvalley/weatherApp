a5560f63084d2d5aac59bbce119bdbadb8fd9606
zaturnvalley <zacharycmason@gmail.com>
2016-12-22 13:42:23 -0800

    google map loader elem & container atr

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { GoogleMapLoader, GoogleMap } from 'react-google-maps';
 
 export default (props) => {
   return (
-    
+    <GoogleMapLoader
+      containerElement={ <div style={{height: '100%'}} /> }
+      />
   );
 }
