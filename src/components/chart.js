a0cdbf4c8b730c75e15dd78a9377be855fab8f08
zaturnvalley <zacharycmason@gmail.com>
2016-12-21 17:16:54 -0800

    cut sparklines from weatherlist, pasted into chart component

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 export default (props) => {
   return (
     <div>
+      <Sparklines height={120} width={180} data={temps}>
+        <SparklinesLine color="red" />
+      </Sparklines>
     </div>
   );
 }
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class WeatherList extends Component {
 
@@ -13,9 +12,7 @@ class WeatherList extends Component {
       <tr key={name}>
         <td>{name}</td>
         <td>
-          <Sparklines height={120} width={180} data={temps}>
-            <SparklinesLine color="red" />
-          </Sparklines>
+
         </td>
       </tr>
     );
