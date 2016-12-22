c0879640a6bf1cf517d2ac03283e615884a080db
zaturnvalley <zacharycmason@gmail.com>
2016-12-21 17:44:19 -0800

    converted kelvin to c

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
@@ -6,14 +7,14 @@ class WeatherList extends Component {
 
   renderWeather(cityData){
     const name = cityData.city.name;
-    const temps = cityData.list.map(weather => weather.main.temp);
+    const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273.15);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
 
     return (
       <tr key={name}>
         <td>{name}</td>
-        <td><Chart data={temps} color="orange" units="K" /></td>
+        <td><Chart data={temps} color="orange" units="C" /></td>
         <td><Chart data={pressures} color="purple" units="hPa" /></td>
         <td><Chart data={humidities} color="blue" units="%"/></td>
       </tr>
@@ -26,7 +27,7 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature (K)</th>
+            <th>Temperature (C)</th>
             <th>Pressure (hPa) </th>
             <th>Humidity (%)</th>
           </tr>
