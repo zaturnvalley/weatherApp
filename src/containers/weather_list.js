bdc367d400552a7940c7f827af86d547f99917a3
zaturnvalley <zacharycmason@gmail.com>
2016-12-21 17:15:54 -0800

    creatd chart comp to put in sparklines

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -7,7 +7,7 @@ class WeatherList extends Component {
   renderWeather(cityData){
     const name = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp);
-    console.log(temps);
+    
 
     return (
       <tr key={name}>
