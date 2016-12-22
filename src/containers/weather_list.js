b7a2aedb0521740c1435f6bcd1dd0e294de07849
zaturnvalley <zacharycmason@gmail.com>
2016-12-21 17:19:03 -0800

    imported chart into weather list, passed in data and color as props

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
 
   renderWeather(cityData){
     const name = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp);
-    
+
 
     return (
       <tr key={name}>
         <td>{name}</td>
         <td>
-
+          <Chart data={temps} color="blue" />
         </td>
       </tr>
     );
