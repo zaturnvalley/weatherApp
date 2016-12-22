e947fea369e1668e6ef33135ecac04126a1226c5
zaturnvalley <zacharycmason@gmail.com>
2016-12-22 13:45:07 -0800

    googlemaps elem pulls in lat and long from api on props

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -5,6 +5,9 @@ export default (props) => {
   return (
     <GoogleMapLoader
       containerElement={ <div style={{height: '100%'}} /> }
+      googleMapElement={
+        <GoogleMap defaultZoom={12} defaultCenter={{lat: props.lat, lng: props.lon}} />
+      }
       />
   );
 }
